5818a862b58dfb5954971f62ed17f247e9e068ed
Evan Cordell <cordell.evan@gmail.com>
2022-08-09 10:37:38 -0400

    add bootstrapping to libctrl

---
+++ bootstrap/crds.go
@@ -0,0 +1,47 @@
+package bootstrap
+
+import (
+	"io/fs"
+	"path"
+	"time"
+
+	v1 "k8s.io/apiextensions-apiserver/pkg/apis/apiextensions/v1"
+	"k8s.io/apimachinery/pkg/util/yaml"
+	"k8s.io/client-go/rest"
+	"sigs.k8s.io/controller-runtime/pkg/envtest"
+)
+
+const (
+	lookaheadBytes         = 100
+	maxCRDInstallTime      = 5 * time.Minute
+	crdInstallPollInterval = 200 * time.Millisecond
+)
+
+// CRD installs the CRDs in the filesystem into the kube cluster configured by the rest config.
+func CRD(restConfig *rest.Config, crdFS fs.ReadDirFS, dir string) error {
+	var crds []*v1.CustomResourceDefinition
+
+	crdFiles, err := crdFS.ReadDir(dir)
+	if err != nil {
+		return err
+	}
+	for _, crdFile := range crdFiles {
+		var crd v1.CustomResourceDefinition
+		file, err := crdFS.Open(path.Join(dir, crdFile.Name()))
+		if err != nil {
+			return err
+		}
+		if err := yaml.NewYAMLOrJSONDecoder(file, lookaheadBytes).Decode(&crd); err != nil {
+			return err
+		}
+		crds = append(crds, &crd)
+	}
+
+	_, err = envtest.InstallCRDs(restConfig, envtest.CRDInstallOptions{
+		CRDs:            crds,
+		MaxTime:         maxCRDInstallTime,
+		PollInterval:    crdInstallPollInterval,
+		CleanUpAfterUse: false,
+	})
+	return err
+}
